2b80adf7e6ef13550d95c0c8998bcd782be6427c
liyuan3970 <857956255@qq.com>
2019-05-18 20:49:23 +0800

    love

--- love/views.py
+++ love/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 # Create your views here.
 def love(request):
-    print("*****************")
+
 
     return render(request,'love.html')
